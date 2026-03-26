04c8517097e88f412e1882bc66f436db47be59df
J-Tshims <jonatshims.pro@gmail.com>
2026-03-26 15:31:51 +0100

    deuxième sauvegarde

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member # Utilise .models car admin.py est dans le même dossier
 
-# On dit à l'admin d'afficher les membres
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    # Les colonnes que tu verras dans la liste
+    list_display = ('full_name', 'english_level', 'is_approved', 'created_at')
+    
+    # Un filtre sur le côté pour voir vite qui est approuvé
+    list_filter = ('is_approved', 'english_level')
+
+# On enregistre le modèle avec sa configuration personnalisée
+admin.site.register(Member, MemberAdmin)
